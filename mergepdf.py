0b7f4d6672c6a8daa8a4f460f6883fbc1bfeb8dc
Shiyi Tan <159016858+shiyitan11@users.noreply.github.com>
2025-07-30 21:10:12 +0100

    mergepdf.py

--- mergepdf.py
+++ mergepdf.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfMerger
 
 merger = PdfMerger()
-for pdf in ["A.pdf", "B.pdf"]:
+for pdf in ["A.pdf", "B.pdf"]: #make sure we have two or more PDF files in the same directory as the script like A.pdf and B.pdf here
     merger.append(pdf)
-merger.write("AB.pdf")
+merger.write("AB.pdf") #After running the code, a new file like AB.pdf here will be created, containing the merged pdf we want
 merger.close()
